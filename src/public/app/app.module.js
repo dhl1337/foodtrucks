f5eb88fdc7c79f5d298946a572cd6ab0e04c9172
Dan Le <dhl1337@gmail.com>
2016-09-15 19:40:23 -0500

    add food truck service and views

--- src/public/app/app.module.js
+++ src/public/app/app.module.js
@@ -1,3 +1,92 @@
 (function () {
-    
+    angular
+        .module('foodtrucksApp', ['ui.router'])
+        .config(['$stateProvider','$urlRouterProvider', config]);
+
+    function config ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('signup', {
+                url: '/signup',
+                templateUrl: '../views/foodtruck/signup/signup.html'
+            })
+            .state('home', {
+                url: '/home',
+                templateUrl: '../views/home.html',
+                controller: 'HomeController',
+                controllerAs: 'home',
+                resolve: {
+                    currentFoodtruck(FoodtruckService) {
+                        return FoodtruckService.getFoodtruck().then(res => {
+                            //console.log('resolve truck success');
+                            return res;
+                        }).catch(function (err) {
+                            //console.error('resolve truck err');
+                            return null;
+                        })
+                    },
+                    currentUser(HomeService) {
+                        return HomeService.getCurrentuser(false).then(res => {
+                            //console.log('Resolve User success');
+                            return res;
+                        }).catch(err => {
+                            //console.log('resolve user failed');
+                            return null;
+                        })
+                    }
+                }
+            })
+            .state('splash', {
+                url: '/welcome',
+                templateUrl: '../views/splash/index.html',
+                controller: 'HomeController',
+                controllerAs: 'home'
+            })
+            .state('home.user', {
+                url: '/user',
+                templateUrl: '../views/splash/user.html',
+                controller: 'ProfileController',
+                controllerAs: 'profile'
+            })
+            .state('home.profile', {
+                url: '/profile/:id',
+                templateUrl: '../views/user/profile/user.html',
+                controller: 'ProfileController',
+                controllerAs: 'profile'
+            })
+            .state('home.profile.reviews', {
+                url: '/reviews',
+                templateUrl: '../views/user/reviews/review.html',
+                controller: 'ProfileController',
+                controllerAs: 'profile'
+            })
+            .state('home.profile.favorites', {
+                url: '/favorites',
+                templateUrl: '../views/user/favorites/favorite.html',
+                controller: 'ProfileController',
+                controllerAs: 'profile'
+            })
+            .state('home.foodtruck', {
+                url: '/foodtruck/:id',
+                templateUrl: '../views/foodtruck/profile/foodtruck.html',
+                controller: 'FoodtruckController',
+                controllerAs: 'foodtruck'
+            })
+            .state('usersettings', {
+                url: '/user_settings/:id',
+                templateUrl: '../views/user/settings/setting.html'
+            })
+            .state('foodtrucksettings', {
+                url: '/foodtruck_settings/:id',
+                templateUrl: '../views/foodtruck/settings/setting.html',
+                controller: 'FoodtruckController',
+                controllerAs: 'foodtruck'
+            })
+            .state('foodtrucksignup', {
+                url: '/foodtruck_setting',
+                templateUrl: '../views/foodtruck/signup/index.html',
+                controller: 'FoodtruckController',
+                controllerAs: 'foodtruck'
+            });
+        $urlRouterProvider.otherwise('/welcome');
+    }
 })();
---
+++ src/public/app/foodtruck/foodtruckService.js
@@ -0,0 +1,32 @@
+(function () {
+    angular
+        .module('foodtrucksApp')
+        .service('foodtruckService', foodtruckService);
+
+    function foodtruckService ($http) {
+
+        this.getFoodtruck = () => {
+            return $http.get('/api/foodtrucks').then(response => response.data);
+        };
+        this.addNewFoodtruck = (foodtruck) => {
+            return $http.post('/api/foodtrucks', foodtruck).then(response => response.data);
+        };
+        this.getFoodtruckId = (id) => {
+            return $http.get('/api/foodtrucks/' + id).then(response => response.data);
+        };
+        this.updateFoodtruck = (id, obj) => {
+            return $http.put('/api/foodtrucks/' + id, obj).then(response => response.data);
+        };
+        this.addReview = (id, obj) => {
+            return $http.post('/api/reviews/' + id, obj).then(response => response.data);
+        };
+        this.addFavorite = (currentUserId, foodtruckId) => {
+            return $http.post('/api/users/favorite/' + currentUserId, foodtruckId).then(response => response.data);
+        };
+        this.sendTextMessage = (data) => {
+            return $http.post('/api/twilio', data).then(response => response.data);
+        };
+
+    }
+
+})();
